9c5b72229acfffef225d0f58522f300462de29e3
Grace Lee <gracelee9901@gmail.com>
2023-01-20 02:59:43 -0800

    Initialize state with default value of array from boxes and map over to display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import boxes from "./boxes";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [boxesData, setBoxesData] = React.useState(boxes);
+    
+    const boxElements = boxesData.map(box => {
+        return (
+            <p key={box.id} className="boxes"></p>
+        )
+    });
 
-export default App;
+    return (
+        <main>
+            {boxElements}
+        </main>
+    );
+}
